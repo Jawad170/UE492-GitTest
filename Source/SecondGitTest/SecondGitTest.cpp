cfaaa35c2390ddca3f4bbf348477f6032654af03
Agon <Jawad_Jandaly@hotmail.com>
2016-02-08 22:35:41 +0400

    Blank Project

---
+++ Source/SecondGitTest/SecondGitTest.cpp
@@ -0,0 +1,5 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "SecondGitTest.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, SecondGitTest, "SecondGitTest" );
---
+++ Source/SecondGitTest/SecondGitTestGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "SecondGitTestGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SECONDGITTEST_API ASecondGitTestGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
